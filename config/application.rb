fb9aabeb0ac5c09a6b509b465aa798671e4a7c1f
Spiros Kabasakalis <kabasakalis@gmail.com>
2015-08-22 16:00:47 +0300

    Pipeline hell

--- config/application.rb
+++ config/application.rb
@@ -22,35 +22,46 @@ module Myapp
      I18n.available_locales = [:el, :en]
       config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
       config.i18n.default_locale = :en
-    # Enable escaping HTML in JSON.
-    config.active_support.escape_html_entities_in_json = true
-    # Enable the asset pipeline
-    config.assets.enabled = true
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
 
-    # Bower asset paths
-    root.join('vendor', 'assets', 'components').to_s.tap do |bower_path|
-      config.sass.load_paths << bower_path
-      config.assets.paths << bower_path
-    end
-
-     config.assets.paths << "#{Rails.root}/app/assets/images/metronic"
-    config.autoload_paths += [
-      config.root.join('app')
-    ]
+    #config.autoload_paths += %W(#{config.root}/app/concerns)
+      config.autoload_paths += [
+    config.root.join('app')
+     ]
+  config.autoload_paths << Rails.root.join('lib')
+  Rails.application.config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 
 config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 #config.autoload_paths += %W(#{config.root}/app/concerns)
 
-    # Precompile Bootstrap fonts
-    config.assets.precompile << %r(bootstrap-sass-official/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff)$)
-    # Minimum Sass number precision required by bootstrap-sass
-    ::Sass::Script::Number.precision = [10, ::Sass::Script::Number.precision].max
+    # Enable escaping HTML in JSON.
+    config.active_support.escape_html_entities_in_json = true
 
  console do
       ARGV.push "-r", root.join("lib/console.rb")
     end
+
+    # # Enable the asset pipeline
+    # config.assets.enabled = true
+    # # Version of your assets, change this if you want to expire all your assets
+    # config.assets.version = '1.0'
+
+    # # Bower asset paths
+    # root.join('vendor', 'assets', 'components').to_s.tap do |bower_path|
+    #   config.sass.load_paths << bower_path
+    #   config.assets.paths << bower_path
+    # end
+
+    #  config.assets.paths << "#{Rails.root}/app/assets/images/metronic"
+    # config.autoload_paths += [
+    #   config.root.join('app')
+    # ]
+
+
+    # # Precompile Bootstrap fonts
+    # config.assets.precompile << %r(bootstrap-sass-official/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff)$)
+    # # Minimum Sass number precision required by bootstrap-sass
+    # ::Sass::Script::Number.precision = [10, ::Sass::Script::Number.precision].max
+
 
 
   end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,10 +1,33 @@
 # Be sure to restart your server when you modify this file.
 
-# Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+  Rails.application.config.assets.enabled = true
+  Rails.application.config.assets.debug = true
+  # Version of your assets, change this if you want to expire all your assets.
+  Rails.application.config.assets.version = '2.0'
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( search.js ie10-viewport-bug-workaround.js)
+  # Bower asset paths
+  Rails.application.root.join('vendor', 'app_components').to_s.tap do |bower_path|
+    Rails.application.config.sass.load_paths << bower_path
+    Rails.application.config.assets.paths << bower_path
+  end
 
-Rails.application.config.assets.precompile += %w( ckeditor/* )
+  Rails.application.config.assets.paths << "#{Rails.root}/app/assets/images/metronic"
+
+
+  # Precompile additional assets.
+  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+  Rails.application.config.assets.precompile += %w( search.js ie10-viewport-bug-workaround.js)
+  Rails.application.config.assets.precompile += %w( ckeditor/* )
+
+
+  # Precompile Bootstrap fonts
+  Rails.application.config.assets.precompile << %r(bootstrap-sass-official/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff)$)
+  # Minimum Sass number precision required by bootstrap-sass
+  ::Sass::Script::Number.precision = [10, ::Sass::Script::Number.precision].max
+
+
+
+
+
+
+
