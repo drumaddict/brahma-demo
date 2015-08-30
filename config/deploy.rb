4d9c123d908c0e7345597f16f67bae1e2282f534
Spiros Kabasakalis <kabasakalis@gmail.com>
2015-08-30 17:02:41 +0300

    Removed database from linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+#set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets  public/system public/uploads public/assets}
