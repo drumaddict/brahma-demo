5de1f47b34398b1abe5ace785b7a7084f71a4ec8
Spiros Kabasakalis <kabasakalis@gmail.com>
2015-08-31 01:26:18 +0300

    smtp try 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
         :address              => "smtp.gmail.com",
         :port                 => 587,
-        :domain               => "gmail.com",
         :user_name            => "kabasakalis@gmail.com",
         :password             => "lauter211171",
         :authentication       => :plain,
