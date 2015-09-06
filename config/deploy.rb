36d08eaa59c3be8b1e863249bb823a03fd55aa78
Spiros Kabasakalis <kabasakalis@gmail.com>
2015-09-06 23:41:57 +0300

    _

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 server '188.226.189.157', user: 'deploy', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@bitbucket.org:drumaddict/app.git'
-#set :application,     'brahma-demo'
+set :application,     'brahma-demo'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
@@ -10,7 +10,7 @@ set :puma_workers,    0
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
-#set :stage,           :production
+set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/var/www/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
