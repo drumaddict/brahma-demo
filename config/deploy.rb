9de985a28caa0e0e0f59b8df73f6e0a04238a8bb
Spiros Kabasakalis <kabasakalis@gmail.com>
2015-09-12 01:25:02 +0300

    fix assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ set :deploy_to, "/var/www/#{fetch(:application)}"
 set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads public/assets_T}
 
 
 # Default value for linked_dirs is []
